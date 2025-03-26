5b15cec42f9d9299349a165434e009cefef60b4c
HephziAtUlster <wilson-h28@ulster.ac.uk>
2025-03-26 23:10:27 +0000

    Update eda.R with Gender
    Gender based EDA covering gender dist, height, weight and risk factors

--- scripts/eda.R
+++ scripts/eda.R
@@ -1,3 +1,6 @@
+# EDA for height, weight, glucose and activity could be useful
+
+
 # install libraries
 install.packages("corrplot")
 install.packages("ggplot2")
@@ -21,6 +24,114 @@ colSums(is.na(data))
 data <- data%>%
   mutate(age_years = age / 365)
 
+# Comparing Gender Distribution
+# Important for ethical considerations, need to consider how this data effects the results and if it's a fair distribution
+# Since there's a far greater number (roughly 65%)of female patients we need to consider why in the report
+ggplot(data, aes(x=factor(gender))) +
+  geom_bar(fill= "steelblue") +
+  xlab("Gender (1 = Women, 2 = Men)") +
+  ggtitle("Gender Distribution of Patients")
+
+# Comparing gender distribution against CVD
+# Very slightly more men have CVD proportionally than women but statistically insignificant
+ggplot(data, aes(x = factor(gender), fill = factor(cardio))) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  xlab("Gender (1 = Women, 2 = Men)") +
+  ylab("Proportion of Patients") +
+  ggtitle("Gender by Cardiovascular Disease Status") +
+  labs(fill = "CVD (0 = No, 1 = Yes)")
+
+# Comparing gender distribution against Age
+# Accounts for difference in number of men and women
+ggplot(data, aes(x = age_years, fill = factor(gender))) +
+  geom_density(alpha = 0.4, adjust = 1.5) +  # Adjusting for a smoother curve
+  xlab("Age (Years)") +
+  ylab("Density") +
+  ggtitle("Normalised Age Distribution against Gender") +
+  labs(fill = "Gender (1 = Women, 2 = Men)")
+
+# Comparing gender distribution against height
+ggplot(data, aes(x = height, fill = factor(gender))) +
+  geom_density(alpha = 0.4, adjust = 1.5) +  # Adjusting for a smoother curve
+  xlab("Height (cm)") +
+  ylab("Density") +
+  ggtitle("Normalised Height Distribution against Gender") +
+  labs(fill = "Gender (1 = Women, 2 = Men)")
+
+# Comparing gender distribution against weight
+ggplot(data, aes(x = weight, fill = factor(gender))) +
+  geom_density(alpha = 0.4, adjust = 1.5) +  # Adjusting for a smoother curve
+  xlab("Weight (Kg)") +
+  ylab("Density") +
+  ggtitle("Normalised Weight Distribution against Gender") +
+  labs(fill = "Gender (1 = Women, 2 = Men)")
+
+# Comparing smoking by gender
+# Smoking is far more prevalent in men
+ggplot(data, aes(x = factor(gender), fill = factor(smoke))) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  xlab("Gender (1 = Women, 2 = Men)") +
+  ylab("Proportion of Patients") +
+  ggtitle("Smoking against Gender") +
+  labs(fill = "Alcohol (0 = No, 1 = Yes)")
+
+# Comparing Alcohol consumption by gender
+# Alcohol consumption is also more prevalent in men, but less so than smoking
+ggplot(data, aes(x = factor(gender), fill = factor(alco))) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  xlab("Gender (1 = Women, 2 = Men)") +
+  ylab("Proportion of Patients") +
+  ggtitle("Alcohol Consumption against Gender") +
+  labs(fill = "Alcohol (0 = No, 1 = Yes)")
+
+# Comparing cholesterol levels against gender
+# Women in the population have a generally higher cholesterol level than men
+ggplot(data, aes(x = factor(gender), fill = factor(cholesterol))) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  xlab("Gender (1 = Women, 2 = Men)") +
+  ylab("Proportion of Patients") +
+  ggtitle("Cholesterol Levels against Gender") +
+  labs(fill = "Cholesterol Level (1 = Normal, 2 = Above Normal, 3 = Well Above Normal)")
+
+# Comparing Systolic and Diastolic blood pressure against gender
+# Not entirely happy about how these look, might be better way to represent
+# However women have a greater max blood pressure than men
+ggplot(data, aes(x = factor(gender), y = ap_hi, fill = factor(gender))) +
+  geom_boxplot() +
+  xlab("Gender (1 = Women, 2 = Men)") +
+  ylab("Systolic Blood Pressure") +
+  ggtitle("Systolic Blood Pressure against Gender")
+
+ggplot(data, aes(x = factor(gender), y = ap_lo, fill = factor(gender))) +
+  geom_boxplot() +
+  xlab("Gender (1 = Women, 2 = Men)") +
+  ylab("Diastolic Blood Pressure") +
+  ggtitle("Diastolic Blood Pressure against Gender")
+
+# Comparing Glucose levels between men and women
+# Women have slightly higher glucose levels
+ggplot(data, aes(x = factor(gender), fill = factor(gluc))) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  xlab("Gender (1 = Women, 2 = Men)") +
+  ylab("Proportion of Patients") +
+  ggtitle("Glucose Levels against Gender") +
+  labs(fill = "Glucose Level (1 = Normal, 2 = Above Normal, 3 = Well Above Normal)")
+
+# Comparing activity by gender
+# Activity levels are very similar
+ggplot(data, aes(x = factor(gender), fill = factor(active))) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  xlab("Gender (1 = Women, 2 = Men)") +
+  ylab("Proportion of Patients") +
+  ggtitle("Activity against Gender") +
+  labs(fill = "Activity (0 = Not Active, 1 = Active)")
+
 # Visualize age distribution
 ggplot(data, aes(x = age_years)) +
   geom_histogram(bins = 30) +
@@ -30,7 +141,7 @@ ggplot(data, aes(x = age_years)) +
 # Visualize distribution of cholesterol levels
 ggplot(data, aes(x = factor(cholesterol))) +
   geom_bar() +
-  xlab("Cholesterol Level (1: Normal, 2: Above Normal, 3: Well Above Normal") +
+  xlab("Cholesterol Level (1: Normal, 2: Above Normal, 3: Well Above Normal)") +
   ggtitle("Cholesterol Level Distribution")
 
 # Correlation analysis of numeric variables
@@ -79,11 +190,12 @@ gg_alcohol <- ggplot(data, aes(x = factor(alco), fill = factor(cardio))) +
 gg_alcohol
 
 # Compare systolic blood pressure distribution for patients
+# Issues here!
 gg_bp <- ggplot(data, aes(x = ap_hi, fill = factor(cardio))) +
   geom_histogram(position = "identity", alpha = 0.5, bins = 30) +
   xlab("Systolic Blood Pressure (ap_hi)") +
   ggtitle("Systolic Blood Pressure Distribution by Disease Status") +
-  labs(fill = "CVD (0 = No, 1 = Yes")
+  labs(fill = "CVD (0 = No, 1 = Yes)")
 
 # Display gg_bp
 gg_bp
